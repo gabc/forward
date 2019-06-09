7f6caba3ea091e498127b3fcb9c688c27767d955
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2019-06-08 20:06:19 -0400

    Run lambda in core words

--- forward.lisp
+++ forward.lisp
@@ -16,10 +16,13 @@
 (defun make-word (name code &optional (core nil))
   (let ((new-word name))
     (setf (get new-word 'name) name)
-    (setf (get new-word 'code) code)
+    (setf (get new-word 'code) (if core (eval `(lambda () ,code)) code))
     (setf (get new-word 'here) (length *dictionary*))
     (setf (get new-word 'core) core) ; Does it get `eval'd or recursively `eval'd?
     (push new-word *dictionary*)))
+
+(defun drop-word (name)
+  (delete (find-word name) *dictionary*))
 
 (defun find-word (name &optional (array-index nil array-index-p))
   (flet ((find-word-from-end (name)
@@ -29,9 +32,9 @@
          (find-word-from (name array-index)
 	   ;; Support redifinition but statically. Not all the word.
            (values name array-index)))
-   (if array-index-p
-       (find-word-from-end name)
-       (find-word-from name array-index))))
+    (if array-index-p
+	(find-word-from-end name)
+	(find-word-from name array-index))))
 
 (defun forth-read ()
   (let ((*readtable* *forth-readtable*))
@@ -47,7 +50,7 @@
 (defun run (word)
   (flet ((run-word (entry)
            (when (get entry 'core)      ; Just run the code
-             (eval (get entry 'code))
+             (funcall (get entry 'code))
              (return-from run))
            (let ((code (get entry 'code)))
              (dolist (word code)
@@ -69,11 +72,12 @@
 (make-word 'q '(setf *exit-flag* t) t)
 (make-word 'code '(print (symbol-plist (find-word (intern (string-upcase (stack-pop)))))) t)
 (make-word '_ '(print (stack-pop)) t)
-;; How to read symbol like ; and : ?
+(make-word '= '(stack-push (= (stack-pop) (stack-pop)) t))
+;(make-word 'if)
 (make-word '|:| '(let (code temp name)
 		  (setf name (forth-read))
 		  (loop while (not (eq (setf temp (forth-read)) '|;|))
-			do (push temp code))
+		     do (push temp code))
 		  (make-word name (nreverse code)))
 	   t)
 
