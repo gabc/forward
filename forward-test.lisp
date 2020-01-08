7134b6392e90984bb98ade44b65026f45bba782b
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2020-01-08 12:36:38 -0500

    Make it so we can run test from string
         This means we can have defines in the tests.

--- forward-test.lisp
+++ forward-test.lisp
@@ -5,20 +5,27 @@
 (5am:in-suite forward-suite)
 
 (defvar foo 4)
-(defun run-list (cmds)
-  (dolist (c cmds)
-    (run c)))
+(defun run-str (str)
+  (with-open-stream (s (make-string-input-stream str))
+    (setf *exit-flag* nil)
+    (handler-case
+	(loop while (not *exit-flag*)
+	   do
+	     (let ((word (forth-read s)))
+               (run word s)))
+      (end-of-file (c)
+	(declare (ignore c))))))
 
 (defmacro runt (cmds)
   `(progn
      (setf *stack* nil)
-     (run-list ',cmds)
+      (run-str ,cmds)
      *stack*))
-(5am:test forward1
-  (5am:is (equal '(2) (runt (1 1 +))))
-  (5am:is (equal '(2 1) (runt (1 1 1 +))))
-  (5am:is (equal '(3) (runt (1 1 1 + +))))
-  (5am:is (equal '(8) (runt (2 2 2 + *))))
-  ;; I can't test : this way tho.
-  ;; (5am:is (equal '(8) (runt (|:| foo 2 2 + |;| 2 foo *))))
-  )
+
+(5am:test
+ forward1
+ (5am:is (equal '(2) (runt "1 1 +")))
+ (5am:is (equal '(2 1) (runt "1 1 1 +")))
+ (5am:is (equal '(3) (runt "1 1 1 + +")))
+ (5am:is (equal '(8) (runt "2 2 2 + *")))
+ (5am:is (equal '(8) (runt ": foo 2 2 + ; 2 foo *"))))
